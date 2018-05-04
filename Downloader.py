c13bcf9548e53f852d27b8665466563d7fc85f33
jonathan <zxcv859500@naver.com>
2018-05-05 00:21:57 +0900

    모든 페이지 다운로드 구현 완료

--- Downloader.py
+++ Downloader.py
@@ -3,74 +3,75 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 class Downloader:
-     def  __init__(self):
-          self.path = "C:\\Users\\조나단\\Desktop\\Test\\"
 
+     #Setting(경로) : 크롬 기본 옵션 설정, 다운로드 경로 설정
+     def Setting(self, path):
           self.options = webdriver.ChromeOptions()
 
           profile = {"plugins.plugins_list": [{"enabled": False, "name": "Chrome PDF Viewer"}],
-                    "download.default_directory": self.path, "download.extensions_to_open": "applications/pdf"}
+                     "download.default_directory": path, "download.extensions_to_oepn": "applications/pdf"}
           self.options.add_experimental_option("prefs", profile)
 
-          #self.options.add_argument('headless')
-          #self.options.add_argument('window')
-          #self.options.add_argument('disable-gpu')
-          '''
-          prefs = {
-               'profile.default_content_seetings.popups' : 0,
-               'download.default_directroy' : 'C:\\Users\\조나단\\Desktop\\',
-               'directory_upgrade' : True,
-               'extensions_to_open' : ""
-          }
-          '''
-          #self.options.add_experimental_option('prefs', prefs)
+          #self.options.add_argument('--no-sandbox')
+          #self.options.add_argument('--headless')
+          #self.options.add_argument('--window-size=1920x1080')
+          #self.options.add_argument('--disable-gpu')
 
+     #GetSearchPage(검색어) : 검색 결과 첫 페이지로 이동
+     def GetSearchPage(self, keyword):
           self.browser = webdriver.Chrome('./chromedriver.exe', chrome_options=self.options)
           self.browser.get('http://www.google.co.kr')
           self.browser.implicitly_wait(3)
 
-
-     def GetSearchPage(self, keyword):
           search_keyword = keyword + ' filetype:pdf'
           search_bar = self.browser.find_element_by_xpath('//*[@id="lst-ib"]')
           search_bar.send_keys(search_keyword)
 
           self.browser.implicitly_wait(3)
-          #self.browser.get_screenshot_as_file('first_screenshot.png')
-
-          #self.browser.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[3]/center/input[1]').click()
           search_bar.send_keys(Keys.ENTER)
           self.browser.implicitly_wait(3)
 
-          self.browser.get_screenshot_as_file('screenshot.png')
-
+     #Donwload() : 페이지 안의 모든 검색 결과를 다운 한 후 다음 검색결과 페이지로 넘어감
      def Download(self):
 
-          for idx in range(1, 11):
-               self.browser.find_element_by_xpath('//*[@id="rso"]/div/div/div[{}]/div/div/h3/a'\
-                                                  .format(idx)).click()
-               time.sleep(1)
+          while(1):
+               link = self.browser.current_url
+               Error_cnt = 0
 
-          self.browser.get_screenshot_as_file('screenshot.png')
+               idx = 1
+               while(1):
+                    if idx == 11:
+                         break
 
-     def SetPath(self, path):
-          pass
+                    if Error_cnt >= 11:
+                         return False
+                    try:
+                         self.browser.find_element_by_xpath('//*[@id="rso"]/div/div/div[{}]/div/div/h3/a'\
+                                                       .format(idx)).click()
+                    except:
+                         self.browser.get(link)
+                         self.browser.implicitly_wait(3)
+                         Error_cnt += 1
+                         idx -= 1
 
-down = Downloader()
-down.GetSearchPage('블랙핑크')
-down.Download()
 
-'''
+                    idx += 1
+
+                    time.sleep(2)
+
+               self.browser.get(link)
+               self.browser.implicitly_wait(3)
+               try:
+                    next_btn = self.browser.find_element_by_xpath('//*[@id="pnnext"]')
+               except:
+                    print('Cannot find next button')
+                    return False
+
+               next_btn.click()
+
+
 if __name__ == '__main__':
-     options = webdriver.ChromeOptions()
-     options.add_argument('headless')
-     options.add_argument('window-size=1920x1080')
-     options.add_argument('disable-gpu')
-
-     browser = webdriver.Chrome('./chromedriver.exe', chrome_options=options)
-     browser.get('http://www.ubuntu.com/')
-     browser.implicitly_wait(3)
-     browser.get_screenshot_as_file('naver_main_headless.png')
-     
-     browser.quit()
-'''
+     down = Downloader()
+     down.Setting('C:\\Users\\조나단\\Desktop\\Test\\')
+     down.GetSearchPage('아니 진자 제발 적은 것좀 나와라')
+     down.Download()
